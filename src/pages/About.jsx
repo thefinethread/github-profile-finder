92f1dfbfa7089a05207587de7ed13e85d773e672
Kartikey Shukla <kartikey8604@gmail.com>
2023-10-23 18:53:25 +0530

    about page and its css

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,24 @@
 const About = () => {
-  return <h2>About</h2>;
+  return (
+    <div className="about-container">
+      <div className="main-container">
+        <h1
+          style={{
+            marginBottom: '20px',
+          }}
+        >
+          Github Profile Finder
+        </h1>
+        <p>Created by Kartikey Shukla</p>
+        <p>
+          Discover GitHub users and explore their profiles with ease. Simply
+          search by username to access detailed information, including avatar,
+          bio, followers, repositories, and more. This is the first version,
+          stay tuned for updates!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
