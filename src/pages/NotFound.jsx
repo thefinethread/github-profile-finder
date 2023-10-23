5093bb01d6c0dbf78306d7c0fdf546e829c0bbe1
Kartikey Shukla <kartikey8604@gmail.com>
2023-10-23 22:17:34 +0530

    not found page and its css

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
+import { RiHome5Line } from 'react-icons/ri';
+
 const NotFound = () => {
-  return <h2>notfound</h2>;
+  return (
+    <div className="not-found-container">
+      <div className="main-container flex">
+        <h1>Oops!</h1>
+        <h3>404 - Page not found!</h3>
+        <Link to="/" className="btn btn-primary flex">
+          <RiHome5Line />
+          Back to Home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
