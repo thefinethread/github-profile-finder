d839f5405f849728bf753e2503c05de76ce427f8
Kartikey Shukla <kartikey8604@gmail.com>
2023-10-23 18:30:44 +0530

    added footer component

--- src/components/Layouts/Footer.jsx
+++ src/components/Layouts/Footer.jsx
@@ -1,5 +1,20 @@
+import { RiGithubFill } from 'react-icons/ri';
+
+const footerStyle = {
+  fontSize: '2rem',
+};
+
 const Footer = () => {
-  return <footer>footer</footer>;
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="flex">
+          <RiGithubFill style={footerStyle} />
+          <div>Copyright &copy; All rights reserved</div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
