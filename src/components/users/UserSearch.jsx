0588d014513f366aef13ce6f46388aa04de21b3d
Kartikey Shukla <kartikey8604@gmail.com>
2023-10-26 22:36:20 +0530

    added text state in user search component and added handle input and handle submit functions

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,15 +1,37 @@
+import { useState } from 'react';
 import { RiCloseCircleFill } from 'react-icons/ri';
 
 const UserSearch = () => {
+  const [text, setText] = useState('');
+
+  const handleInput = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (text.trim().length === 0) {
+      alert('Please enter username');
+    }
+  };
+
   return (
     <div className="search-container">
       <div>
-        <form className="flex">
+        <form className="flex" onSubmit={handleSubmit}>
           <div className="form-control">
-            <input type="text" placeholder="search username..." />
-            <button className="clear-input-btn">
-              <RiCloseCircleFill size="1.4rem" />
-            </button>
+            <input
+              type="text"
+              placeholder="search username..."
+              value={text}
+              onChange={handleInput}
+            />{' '}
+            {text.length > 0 && (
+              <button type="button" className="clear-input-btn">
+                <RiCloseCircleFill size="1.4rem" />
+              </button>
+            )}
           </div>
           <button className="btn btn-primary" type="submit">
             Search
