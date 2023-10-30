55d8a97bd5d7ed7eea50b4fe742b4358976ae462
Kartikey Shukla <kartikey8604@gmail.com>
2023-10-30 23:50:16 +0530

    added connections section, and extra info section  and its css in left section of user component

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,5 +1,12 @@
 import { useContext, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
+import {
+  RiGroupLine,
+  RiMapPin2Line,
+  RiMailLine,
+  RiLinksLine,
+  RiTwitterXFill,
+} from 'react-icons/ri';
 import GithubContext from '../contexts/github/GithubContext';
 import Spinner from '../components/shared/Spinner';
 
@@ -11,36 +18,126 @@ const User = () => {
     getUser(params.login);
   }, []);
 
+  const roundOffConnections = (num) => {
+    return num < 1000 ? num : (num / 1000).toFixed(1) + 'k';
+  };
+
   if (isLoading) {
     return <Spinner />;
   }
 
-  return (
-    <div className="container">
-      <div style={{ marginBottom: '10px' }}>
-        <Link to="/">Back to search</Link>
-      </div>
-      <div className="divider"></div>
-      {user !== null && (
-        <div className="user-container flex">
-          <div className="left-section flex">
-            <div className="profile-pic-container">
-              <img src={user.avatar_url} alt="profile pic" />
-            </div>
-            <div className="user-name">
-              <h2>{user.name}</h2>
-              <p className="text-dim">{user.login}</p>
-            </div>
-            <button type="button" className="btn btn-secondary">
-              Visit Github Profile
-            </button>
-            <div className="bio"> {user.bio}</div>
-          </div>
-          <div className="right-section">repos</div>
+  if (user !== null) {
+    const {
+      name,
+      login,
+      avatar_url,
+      html_url,
+      type,
+      hireable,
+      bio,
+      followers,
+      following,
+      followers_url,
+      following_url,
+      location,
+      blog,
+      twitter_username,
+      email,
+    } = user;
+
+    return (
+      <div className="container">
+        <div style={{ marginBottom: '10px' }}>
+          <Link to="/">Back to search</Link>
         </div>
-      )}
-    </div>
-  );
+        <div className="divider"></div>
+        {user !== null && (
+          <div className="user-container flex">
+            <div className="left-section flex">
+              <div className="profile-pic-container">
+                <img src={avatar_url} alt="profile pic" />
+              </div>
+              <div className="user-name">
+                <h2>{name}</h2>
+                <p className="text-dim">{login}</p>
+              </div>
+              <div className="user-tags flex text-sm-2">
+                <span>{type}</span>
+                <span>{hireable && 'Hireable'}</span>
+              </div>
+              <a
+                className="width-100-prct"
+                href={html_url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button className="btn btn-secondary width-100-prct">
+                  Visit Github Profile
+                </button>
+              </a>
+              <div className="bio">{bio}</div>
+              <div className=" flex text-sm flex-gap-5">
+                <a
+                  href={following_url}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="flex flex-gap-5"
+                >
+                  <RiGroupLine className="text-dim" />
+                  <span>{roundOffConnections(followers)}</span>
+                  <span className="text-dim"> followers</span>
+                </a>
+                <span style={{ fontSize: '5px' }}>&#x2022;</span>
+                <a
+                  href={followers_url}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="flex flex-gap-5"
+                >
+                  <span>{roundOffConnections(following)}</span>
+                  <span className="text-dim">followers</span>
+                </a>
+              </div>
+              <div>
+                {location && (
+                  <div className="flex flex-jus-ct-strt flex-gap-5">
+                    <RiMapPin2Line className="text-dim" />
+                    <span className="text-sm">{location}</span>
+                  </div>
+                )}
+
+                {email && (
+                  <div className="flex flex-jus-ct-strt flex-gap-5">
+                    <RiMailLine className="text-dim" />
+                    <span className="text-sm">{email}</span>
+                  </div>
+                )}
+
+                {blog && (
+                  <div className="flex flex-jus-ct-strt flex-gap-5">
+                    <RiLinksLine className="text-dim" />
+                    <a href={`https://${blog}`}>
+                      <span className=" text-sm">{blog}</span>
+                    </a>
+                  </div>
+                )}
+
+                {twitter_username && (
+                  <div className="flex flex-jus-ct-strt flex-gap-5">
+                    <RiTwitterXFill className="text-dim" />
+                    <a href={`https://twitter.com/${twitter_username}`}>
+                      <span className=" text-sm">@{twitter_username}</span>
+                    </a>
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="right-section">repos</div>
+          </div>
+        )}
+      </div>
+    );
+  }
 };
 
 export default User;
